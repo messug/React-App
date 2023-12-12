4127bb583a2dd1f2819b18a7ea25739f8657b792
Meseret <mesiteklu2018@gmail.com>
2023-12-11 23:46:15 -0700

    added codes

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,7 +1,7 @@
-import Firebase  from "./firebase"
+import firebase  from "./firebase/app"
 import "firebase/auth"
 
-const app = firebase.initializeApp({
+const app = firebase.initializeApp( {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY, 
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_DATABASE,
@@ -9,7 +9,7 @@ const app = firebase.initializeApp({
   messagingSenderId:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   appId:process.env. REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   measurementId: process.env.REACT_APP_FIREBASE_APP_ID
-})
+});
 
 export const auth = app.auth()
 
